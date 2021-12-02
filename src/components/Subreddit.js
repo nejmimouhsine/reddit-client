4b77b14d7ced49ef186dfb285453dd2456fc5662
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-12-02 14:21:05 +0100

    Add see more to subreddit

--- src/components/Subreddit.js
+++ src/components/Subreddit.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { /*useDispatch,*/ useSelector } from 'react-redux';
 import { selectPosts } from '../features/Posts/postsSlice';
 
 const Subreddit = () => {
   const [visible, setVisible] = useState(5);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
   const subreddits = Object.values(posts).map(({ subreddit }) => subreddit);
   const uniqueSubreddits = [...new Set(subreddits)];
 
-  const handleClick = (e) => dispatch((e.target.id));
+  const handleClick = (e) => console.log(e.target.id);
   const seeMore = () => setVisible(uniqueSubreddits.length);
 
   return (
     <div>
       <h2>Subreddit</h2>
-      {uniqueSubreddits.slice(0, visible).map((subreddit) => (
-        <ul style={{ margin: '0 20px' }} key={subreddit}>
-          <li onClick={handleClick} id={subreddit}>
-            {subreddit}
-          </li>
-        </ul>
-      ))}
-      {visible > 5 ? null : <button onClick={seeMore}>Show More</button>}
+      {uniqueSubreddits &&
+        uniqueSubreddits.slice(0, visible).map((subreddit) => (
+          <ul style={{ margin: '0 20px' }} key={subreddit}>
+            <li onClick={handleClick} id={subreddit}>
+              {subreddit}
+            </li>
+          </ul>
+        ))}
+      {uniqueSubreddits.length ? (
+        visible > 5 ? null : (
+          <button onClick={seeMore}>See More</button>
+        )
+      ) : null}
     </div>
   );
 };

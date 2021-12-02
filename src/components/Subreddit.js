64d381a2e6cf4c9ba42dbe9eaaeb2dd676011652
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-12-02 14:13:54 +0100

    Add See more Functionnality to subreddit

--- src/components/Subreddit.js
+++ src/components/Subreddit.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectPosts } from '../features/Posts/postsSlice';
 
 const Subreddit = () => {
+  const [visible, setVisible] = useState(5);
+  const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
   const subreddits = Object.values(posts).map(({ subreddit }) => subreddit);
   const uniqueSubreddits = [...new Set(subreddits)];
 
-  const handleClick = (e) => console.log(e.target.id);
+  const handleClick = (e) => dispatch((e.target.id));
+  const seeMore = () => setVisible(uniqueSubreddits.length);
 
   return (
     <div>
       <h2>Subreddit</h2>
-      {uniqueSubreddits.map((subreddit) => (
-        <div key={subreddit}>
-          <p onClick={handleClick} id={subreddit}>
+      {uniqueSubreddits.slice(0, visible).map((subreddit) => (
+        <ul style={{ margin: '0 20px' }} key={subreddit}>
+          <li onClick={handleClick} id={subreddit}>
             {subreddit}
-          </p>
-        </div>
+          </li>
+        </ul>
       ))}
+      {visible > 5 ? null : <button onClick={seeMore}>Show More</button>}
     </div>
   );
 };
